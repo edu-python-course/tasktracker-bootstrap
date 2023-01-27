89569e6a956be633eeffecc31ba634c353b3f59e
Serhii Horodilov <sgorodil@gmail.com>
2023-01-27 05:13:12 +0200

    Added base comment hadling

--- js/tasktracker.task.js
+++ js/tasktracker.task.js
@@ -44,9 +44,62 @@ const toggleTaskStatus = event => {
 }
 
 
+const createCommentEntry = ({author, posted, message}) => {
+    const containerClassList = ["comment-author", "comment", "d-flex", "mb-2"];
+
+    const commentContainer = document.createElement("div");
+    commentContainer.classList.add(...containerClassList);
+
+    const avatar = document.createElement("img");
+    commentContainer.appendChild(avatar);
+    avatar.classList.add("rounded-circle", "shadow", "mx-3");
+    avatar.src = author.img;
+
+    const comment = document.createElement("div");
+    commentContainer.appendChild(comment);
+    comment.classList.add("card", "shadow", "px-5", "py-2");
+
+    const commentAuthor = document.createElement("div");
+    comment.appendChild(commentAuthor);
+    commentAuthor.classList.add("author", "h5", "mb-2");
+    commentAuthor.innerText = author.first_name + " " + author.last_name;
+
+    const commentPosted = document.createElement("small");
+    comment.appendChild(commentPosted);
+    commentPosted.classList.add("small");
+    commentPosted.innerText = posted;
+
+    const commentMessage = document.createElement("p");
+    comment.appendChild(commentMessage);
+    commentMessage.innerText = message;
+
+    return commentContainer;
+}
+
+
+const addComment = (comment) => {
+    const container = document.getElementById("commentsContainer");
+    container.insertBefore(comment, container.firstChild);
+};
+
+
 const handleSubmitComment = event => {
     event.preventDefault();
     event.stopPropagation();
+
+    const message = document.querySelector("#commentForm input").value;
+    event.target.reset();
+    const payload = {
+        "author": {
+            "id": "598da43b",
+            "img": "https://i.pravatar.cc/350?u=598da43b",
+            "first_name": "Marmadoc",
+            "last_name": "Brockhouse",
+        },
+        "posted": "Jan 27, 2023 04:45",
+        message
+    }
+    addComment(createCommentEntry(payload));
 }
 
 
@@ -57,4 +110,4 @@ const taskActionButton = document.querySelector("#taskActionsContainer .btn.task
 if (taskActionButton) taskActionButton.addEventListener("click", toggleTaskStatus);
 
 const commentForm = document.getElementById("commentForm");
-if (commentForm) commentForm.addEventListener("submit", handleSubmitComment)
+if (commentForm) commentForm.addEventListener("submit", handleSubmitComment);
