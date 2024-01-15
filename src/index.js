87f6e7c39b1d6330bd057eed751e7ebcb94b0f48
Serhii Horodilov <sgorodil@gmail.com>
2024-01-15 05:06:10 +0200

    Set up webpack

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// noinspection ES6UnusedImports
+
+/**
+ * Webpack default entry point
+ */
+
+// include bootstrap sources
+import "bootstrap-icons/font/bootstrap-icons.scss"
+import "bootstrap/scss/bootstrap.scss"
+import * as bootstrap from "bootstrap"
---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+/**
+ * Webpack configuration
+ *
+ * This configuration uses the default entry point for webpack v5 at
+ * "src/index.js" (omitted inside the config object)
+ *
+ */
+
+"use strict"
+
+const path = require("path")
+const autoprefixer = require("autoprefixer")
+const HTMLWebpackPlugin = require("html-webpack-plugin")
+const MiniCSSExtractPlugin = require("mini-css-extract-plugin")
+
+// webpack config object
+// noinspection WebpackConfigHighlighting
+module.exports = {
+    mode: "development",
+    output:{
+        filename: "js/main.bundle.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true
+    },
+    devServer: {
+        static: path.resolve(__dirname, "dist"),
+        port: 3000,
+        hot: true
+    },
+    plugins: [
+        new MiniCSSExtractPlugin({filename:"css/main.min.css"}),
+        new HTMLWebpackPlugin({
+            template: path.resolve(__dirname, "src/views/_template.hbs"),
+            filename: "index.html"
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    {loader: MiniCSSExtractPlugin.loader},
+                    {loader: "css-loader"},
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                plugins: [autoprefixer]
+                            }
+                        }
+                    },
+                    {loader: "sass-loader"}
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+                generator: {
+                    filename: "img/[name][ext]"
+                }
+            },
+            {
+                mimetype: "image/svg+xml",
+                scheme: "data",
+                type: "asset/resource",
+                generator: {
+                    filename: "icons/[hash].svg"
+                }
+            },
+            {
+                test: /\.hbs$/,
+                loader: "handlebars-loader"
+            }
+        ]
+    }
+}
